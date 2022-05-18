de73e6226bd3cdfb3eb4842fd16801f3f04729ca
nathysanti <nathalia.software@gmail.com>
2022-05-18 10:04:06 -0300

    :sparkles: feat: Criando as funções de soma, multiplicação e divisão

---
+++ Javascript/Funções/funcoes.js
@@ -0,0 +1,20 @@
+function soma(x,y){
+    total = x + y;
+    console.log("O valor da soma de " + x + " + " + y + " é de: " + total);
+    return total; 
+}
+function mult(x,y){
+    total = x * y;
+    console.log("O valor da multiplicação de " + x + " X " + y + " é de: " + total);
+    return total; 
+}
+function divi(x,y){
+    total = x / y;
+    console.log("O valor da divisão de " + x + " / " + y + " é de: " + total);
+    return total; 
+}
+x = 5;
+y = 6;
+soma(x,y);
+mult(x,y);
+divi(x,y);

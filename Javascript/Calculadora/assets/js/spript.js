c4d8ebb1a391ea6cacf0a9ff1df48fed6ef0f6e1
nathysanti <nathalia.software@gmail.com>
2022-05-19 18:50:15 -0300

    :sparkles: feat: adicionando as opções da calc

---
+++ Javascript/Calculadora/assets/js/spript.js
@@ -0,0 +1,6 @@
+function calculadora(){
+    const operacao = prompt('Escolha sua operação: \n 1 - Soma(+) \n 2 - Subtração(-) \n 3 - Multiplicação(*) \n 4 - Divisão Real(/) \n 5 - Divisão Interia(%) \n 6 - Potenciação(**) \n ');
+    
+}
+
+calculadora();

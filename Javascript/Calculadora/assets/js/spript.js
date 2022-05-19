dcd5791ffca30305518a8711c9cc9c9708d1981d
nathysanti <nathalia.software@gmail.com>
2022-05-19 19:10:32 -0300

    :sparkles: feat: adicionando as funções

--- Javascript/Calculadora/assets/js/spript.js
+++ Javascript/Calculadora/assets/js/spript.js
@@ -1,6 +1,53 @@
 function calculadora(){
-    const operacao = prompt('Escolha sua operação: \n 1 - Soma(+) \n 2 - Subtração(-) \n 3 - Multiplicação(*) \n 4 - Divisão Real(/) \n 5 - Divisão Interia(%) \n 6 - Potenciação(**) \n ');
+    const operacao = Number(prompt('Escolha sua operação: \n 1 - Soma(+) \n 2 - Subtração(-) \n 3 - Multiplicação(*) \n 4 - Divisão Real(/) \n 5 - Divisão Interia(%) \n 6 - Potenciação(**) \n '));
     
+
+
+
+
+let n1 = Number(prompt('Informe o primeiro número: '));
+let n2 = Number(prompt('Informe o segundo número: '));
+let resultado;
+
+function soma(){
+    resultado = n1 + n2;
+    alert(`${n1} + ${n2} = ${resultado}`);
+}
+function subtracao(){
+    resultado = n1 - n2;
+    alert(`${n1}  ${n2} = ${resultado}`);
+}
+function multiplicacao(){
+    resultado = n1 * n2;
+    alert(`${n1} * ${n2} = ${resultado}`);
+}
+function divisaoReal(){
+    resultado = n1 / n2;
+    alert(`${n1} / ${n2} = ${resultado}`);
+}
+function divisaoInteira(){
+    resultado = n1 % n2;
+    alert(`${n1} % ${n2} = ${resultado}`);
+}
+function potenciacao(){
+    resultado = n1 ** n2;
+    alert(`${n1} ** ${n2} = ${resultado}`);
+}
+
+
+if (operacao == 1){
+    soma();
+} else if(operacao == 2){
+    subtracao();
+}else if(operacao == 3){
+    multiplicacao()
+}else if(operacao == 4){
+    divisaoReal()
+}else if(operacao == 5){
+    divisaoInteira();
+}else if(operacao ==6){
+    potenciacao();
+}
 }
 
 calculadora();

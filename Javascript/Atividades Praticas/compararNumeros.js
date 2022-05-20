fc5caa9bb739d6ba28e48d80b8ebe55c7a8099cc
nathysanti <nathalia.software@gmail.com>
2022-05-20 11:24:38 -0300

    :sparkles: feat: adicionando a função soma

--- Javascript/Atividades Praticas/compararNumeros.js
+++ Javascript/Atividades Praticas/compararNumeros.js
@@ -10,7 +10,19 @@ function compararNumero(x,y){
         return "não são iguais";
     }  
 }
+function soma(x,y){
+    soma = Number(x + y);
+        if (soma < 10 && soma < 20){
+        // console.log (`Sua soma é de ${soma}, que é menor que 10 e menor que 20.`);
+        return `Sua soma é de ${soma}, que é menor que 10 e menor que 20.`;
+        }else{
+            // console.log(`Sua soma é ${soma}, que não é menor que 10 e não é menor que 20. `);
+            return `Sua soma é ${soma}, que não é menor que 10 e não é menor que 20. `; 
+        }
+    
+}
 
 
-console.log(`Os numeros ${x} e ${y} ${compararNumero(x,y)}`);
+
+console.log(`Os numeros ${x} e ${y} ${compararNumero(x,y)}. ${soma(x,y)}`);
 
